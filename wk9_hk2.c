ca6fc0d911f5bcfe425022c2c484e16d5420fc58
williamyen <95861867+williamyen0623@users.noreply.github.com>
2022-01-05 22:38:37 +0800

    Add files via upload

---
+++ wk9_hk2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void){
+    float num;
+    num=9876.12345;
+    printf("%.1g\n",num);
+    printf("%.2g\n",num);
+    printf("%.3g\n",num);
+    printf("%.4g\n",num);
+    printf("%.5g\n",num);
+    printf("%.6g\n",num);
+    printf("%.7g\n",num);
+    printf("%.8g\n",num);
+    printf("%.9g\n",num);
+    return 0;
+}
