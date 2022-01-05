32422fc74a1fde98344873b61cbda1ee1da06572
williamyen <95861867+williamyen0623@users.noreply.github.com>
2022-01-05 19:52:49 +0800

    Add files via upload

---
+++ wk9_hk3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void){
+    int row,cell;
+    scanf("%d",&cell);
+    if(cell<=702){
+        row=1;
+    }
+    else{
+        row=cell/702+1;
+    }
+    printf("%d-",row);
+    int column=cell%702;
+    if(column<=26){
+        printf("%c",column+64);
+    }
+    else{
+        printf("%c",column/26+64);
+        printf("%c",column%26+64);
+    }
+    return 0;
+}
