f4f75612d56a491e48bf50cac2c413fe79767dc5
williamyen <95861867+williamyen0623@users.noreply.github.com>
2022-01-06 19:00:19 +0800

    Add files via upload

---
+++ wk9_hk5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char const *argv[])
+{
+    char str[11];
+    printf("input>");
+    fgets(str,11,stdin);
+    if(str[strlen(str)-1]=='\n')str[strlen(str)-1]='\0';
+    printf("--output--\n");
+    printf("\"%s\"\n",str);
+    printf("\\%-20s\\\n",str);
+    printf("\\%20s\\\n",str);
+
+    return 0;
+}
